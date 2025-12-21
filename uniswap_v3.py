ecdb870f281d57fee487365d3b277e16218f4b61
bonzoholda <bhumibeka@gmail.com>
2025-12-21 20:20:55 +0700

    Update uniswap_v3.py

--- uniswap_v3.py
+++ uniswap_v3.py
@@ -3,6 +3,8 @@ from web3 import Web3
 from config import RPC_URL, PRIVATE_KEY, WALLET_ADDRESS, SLIPPAGE
 from uniswap_abi import SWAP_ROUTER_ABI, ERC20_ABI
 from config import UNISWAP_V3_ROUTER, USDC
+from uniswap_pool import get_sqrt_price_limit
+
 
 class UniswapV3Client:
     def __init__(self):
@@ -29,46 +31,56 @@ class UniswapV3Client:
         self.w3.eth.send_raw_transaction(signed.rawTransaction)
         time.sleep(3)
 
-    def swap_exact_input(
-        self,
+def swap_exact_input(
+    self,
+    token_in,
+    token_out,
+    pool_address,
+    amount_in,
+    fee=3000,
+    is_buy=True
+):
+    token_in = Web3.to_checksum_address(token_in)
+    token_out = Web3.to_checksum_address(token_out)
+
+    erc20 = self.w3.eth.contract(token_in, abi=ERC20_ABI)
+    decimals = erc20.functions.decimals().call()
+    amount_in_wei = int(amount_in * (10 ** decimals))
+
+    min_out = int(amount_in_wei * (1 - SLIPPAGE))
+
+    self._approve(token_in, amount_in_wei)
+
+    sqrt_price_limit = get_sqrt_price_limit(
+        self.w3,
+        pool_address,
+        max_bps=30,   # 0.30%
+        is_buy=is_buy
+    )
+
+    params = (
         token_in,
         token_out,
-        amount_in,
-        fee=3000
-    ):
-        token_in = Web3.to_checksum_address(token_in)
-        token_out = Web3.to_checksum_address(token_out)
+        fee,
+        WALLET_ADDRESS,
+        int(time.time()) + 45,  # SHORT DEADLINE
+        amount_in_wei,
+        min_out,
+        sqrt_price_limit
+    )
 
-        erc20 = self.w3.eth.contract(token_in, abi=ERC20_ABI)
-        decimals = erc20.functions.decimals().call()
+    tx = self.router.functions.exactInputSingle(params).build_transaction({
+        "from": WALLET_ADDRESS,
+        "nonce": self.w3.eth.get_transaction_count(WALLET_ADDRESS),
+        "gas": 320000,
+        "gasPrice": self.w3.eth.gas_price
+    })
 
-        amount_in_wei = int(amount_in * (10 ** decimals))
-        min_out = int(amount_in_wei * (1 - SLIPPAGE))
+    signed = self.account.sign_transaction(tx)
+    return self.w3.to_hex(
+        self.w3.eth.send_raw_transaction(signed.rawTransaction)
+    )
 
-        self._approve(token_in, amount_in_wei)
-
-        params = (
-            token_in,
-            token_out,
-            fee,
-            WALLET_ADDRESS,
-            int(time.time()) + 60,
-            amount_in_wei,
-            min_out,
-            0
-        )
-
-        tx = self.router.functions.exactInputSingle(params).build_transaction({
-            "from": WALLET_ADDRESS,
-            "nonce": self.w3.eth.get_transaction_count(WALLET_ADDRESS),
-            "gas": 350000,
-            "gasPrice": self.w3.eth.gas_price,
-            "value": 0
-        })
-
-        signed = self.account.sign_transaction(tx)
-        tx_hash = self.w3.eth.send_raw_transaction(signed.rawTransaction)
-        return self.w3.to_hex(tx_hash)
 
     # === Convenience Wrappers ===
 
