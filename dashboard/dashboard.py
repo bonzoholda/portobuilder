b85c5ed53c81f376804cf6d72c3d1bcc0cf79cf1
bonzoholda <bhumibeka@gmail.com>
2025-12-23 11:04:26 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -5,7 +5,11 @@ import time
 import os
 
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder=os.path.join(BASE_DIR, "templates"),
+    static_folder=os.path.join(BASE_DIR, "static")
+)
 
 # Simple and reliable for same-container setups
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
